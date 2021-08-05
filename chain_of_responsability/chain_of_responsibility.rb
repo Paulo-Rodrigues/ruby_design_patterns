b11ff66e3740c6c7f110c00b99a9c256171f0a31
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-08-04 21:53:17 -0300

    chain of responsibility

---
+++ chain_of_responsability/chain_of_responsibility.rb
@@ -0,0 +1,49 @@
+# Chain of responsibility pattern example.
+
+class ChainOfResponsibility
+  def initialize(chain)
+    @chain = chain
+  end
+
+  def handle(request)
+    @chain.each do |handler|
+      handler.call(request)
+      break if request.handled?
+    end
+  end
+end
+
+class Request
+  attr_reader :handled
+
+  def initialize
+    @handled = false
+  end
+
+  def handle
+    @handled = true
+  end
+end
+
+class Handler1
+  def call(request)
+    puts "Handler1 handling request"
+  end
+end
+
+class Handler2
+  def call(request)
+    puts "Handler2 handling request"
+  end
+end
+
+class Handler3
+  def call(request)
+    puts "Handler3 handling request"
+  end
+end
+
+request = Request.new
+chain = ChainOfResponsibility.new([Handler1.new, Handler2.new, Handler3.new])
+chain.handle(request)
+puts "Request handled: #{request.handled}"
